536ca80bc9b581788ab756eeae4a2490454bd6fb
Chinmaya Garg <chinmaya.garg1997@gmail.com>
2026-03-23 22:59:59 -0300

    CORS Enabled

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,13 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ["http://localhost:3001", "http://127.0.0.1:3001"],
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
